b9401c8e9c0ed43f6320c03ed788b30d19bbdf1d
James <jamesmartins671@gmail.com>
2026-01-22 12:04:13 -0300

    mudança de codigo adicional no jogo

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -36,6 +36,15 @@ function verificarChute() {
     }
 }
 
+if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR';
+        utterance.rate = 1.2;
+        window.speechSynthesis.speak(utterance);
+} else {
+    console.log("Web Speech API não suportada neste navegador.");
+}
+
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
